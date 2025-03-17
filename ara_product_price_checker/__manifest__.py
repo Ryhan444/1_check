36276489e92001d9a39dba035cd40a885d681313
Ryhan444 <78718540+Ryhan444@users.noreply.github.com>
2025-03-17 12:30:37 +0700

    [upd] optimaze

--- ara_product_price_checker/__manifest__.py
+++ ara_product_price_checker/__manifest__.py
@@ -37,7 +37,7 @@
     },   
     "installable": True,
     "application": True,
-    "price"                 :  52.10,
+    "price"                 :  53.10,
     "currency"              :  "USD",
     "pre_init_hook"         :  "pre_init_check",
 }
